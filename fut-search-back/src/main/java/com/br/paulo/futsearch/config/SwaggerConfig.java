95609dea77d26af282236925bc3d3477c880b092
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2023-05-07 03:11:58 -0300

    Adding a description in Swagger

--- fut-search-back/src/main/java/com/br/paulo/futsearch/config/SwaggerConfig.java
+++ fut-search-back/src/main/java/com/br/paulo/futsearch/config/SwaggerConfig.java
@@ -12,14 +12,19 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("RESTful API with Java 17 and Spring Boot 3")
+                        .title("RESTful API Fifa Search")
                         .version("v1")
-                        .description("Some description about your API")
+                        .description("A RESTful API containing 4 end-points (Card, TypeCard, Club & Nation)" +
+                                " Where Card is player information and attributes; " +
+                                "TypeCard associating the fifa version with the respective " +
+                                "card type and the corresponding URL; " +
+                                "Club takes the club name and URL corresponding to the symbol; " +
+                                "Nation follows the same idea as Club, but with existing nationalities.")
                         .termsOfService("https://www.openapis.org/")
                         .license(
                                 new License()
                                         .name("Apache 2.0")
-                                        .url("https://twitter.com/home")
+                                        .url("")
                         )
                 );
 
