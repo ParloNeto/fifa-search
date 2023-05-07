fd01ca95eda0d8bc3ff243964c4f006afd74d481
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2023-05-07 03:00:40 -0300

    Finishing customizing the Swagger (CardResource)

--- fut-search-back/src/main/java/com/br/paulo/futsearch/resources/CardResource.java
+++ fut-search-back/src/main/java/com/br/paulo/futsearch/resources/CardResource.java
@@ -26,6 +26,8 @@ public class CardResource {
     private CardService service;
 
 
+
+    @GetMapping
     @Operation(summary = "Finds all Cards", description = "Finds all Cards",
             tags = {"Card"},
             responses = {
@@ -42,13 +44,14 @@ public class CardResource {
                     @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
             }
     )
-    @GetMapping
     public ResponseEntity<List<CardVO>> findAll(){
         List<CardVO> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping("/{id}")
     @Operation(summary = "Finds a Card", description = "Finds a Card",
-            tags = {"People"},
+            tags = {"Card"},
             responses = {
                     @ApiResponse(description = "Success", responseCode = "200",
                             content = {
@@ -61,33 +64,68 @@ public class CardResource {
                     @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
             }
     )
-    @GetMapping("/{id}")
     public ResponseEntity<CardVO> findById(@PathVariable String id){
         CardVO obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
-    @Operation(summary = "Create a new Card",
-            description = "Create a new Card")
+
     @PostMapping
+    @Operation(summary = "Create a new Card",
+            description = "Create a new Card",
+            tags = {"Card"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(schema = @Schema(implementation = CardVO.class))
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
     public ResponseEntity<CardVO> create(@RequestBody CardVO card) {
         CardVO obj = service.create(card);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable String id) {
-        service.delete(id);
-        return ResponseEntity.noContent().build();
-    }
-
     @PutMapping("/{id}")
+    @Operation(summary = "Edit a existing Card",
+    description = "Edit a existing Card",
+    tags = {"Card"},
+    responses = {
+            @ApiResponse(description = "Success", responseCode = "200",
+                    content = {
+                            @Content(schema = @Schema(implementation = CardVO.class))
+                    }),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+    })
     public ResponseEntity<Void> update(@RequestBody CardVO card, @PathVariable String id) {
         CardVO obj = service.update(card);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.noContent().build();
     }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a Card",
+            description = "Delete a card",
+            tags = {"Card"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 
 
 
