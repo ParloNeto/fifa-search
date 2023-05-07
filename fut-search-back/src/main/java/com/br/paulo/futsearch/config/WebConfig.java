5d1a04dd2fc2197374babbaec948c3a0f8f44318
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2023-05-06 22:23:09 -0300

    Update WebConfig.java

--- fut-search-back/src/main/java/com/br/paulo/futsearch/config/WebConfig.java
+++ fut-search-back/src/main/java/com/br/paulo/futsearch/config/WebConfig.java
@@ -1,2 +1,22 @@
-package com.br.paulo.futsearch.config;public class WebConfig {
+package com.br.paulo.futsearch.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("http://localhost:4200")
+                .allowCredentials(true);
+    }
+
+
 }
