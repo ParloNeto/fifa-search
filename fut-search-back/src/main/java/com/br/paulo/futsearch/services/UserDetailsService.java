c683bab9cf44b5cdee9ee891d3654236b28ada23
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2023-09-19 01:53:49 -0300

    Feature: UserDetails implemented

---
+++ fut-search-back/src/main/java/com/br/paulo/futsearch/services/UserDetailsService.java
@@ -0,0 +1,8 @@
+package com.br.paulo.futsearch.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface UserDetailsService {
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+}
---
+++ fut-search-back/src/main/java/com/br/paulo/futsearch/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.br.paulo.futsearch.services;
+
+import com.br.paulo.futsearch.domain.auth.User;
+import com.br.paulo.futsearch.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Nennhum usuário foi encontrado com o nome: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+}
