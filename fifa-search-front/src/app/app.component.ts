092df7e98c71ac26c494895692e46c59aa9ecf8b
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2024-02-10 03:09:49 -0300

    Removing sidebar from the application

--- fifa-search-front/src/app/app.component.ts
+++ fifa-search-front/src/app/app.component.ts
@@ -4,39 +4,8 @@ import { MatSidenav } from '@angular/material/sidenav';
 @Component({
   selector: 'app-root',
   template: `
-
-<mat-sidenav-container autosize>
-  <mat-sidenav class="slideInLeft" mode="over" opened="false">
-    <img
-      src="assets/images/logo-fut-search.png"
-      alt="Logo Fut Search Sidenav"
-    />
-    <span class="entry">
-      <app-fut-add-card-button></app-fut-add-card-button>
-    </span>
-  </mat-sidenav>
-  <mat-sidenav-content>
-    <button
-      id="button-menu-sidebar"
-      (click)="sidenav.toggle()"
-      mat-icon-button
-      aria-label="Menu icon"
-    >
-      <mat-icon>menu</mat-icon>
-    </button>
     <router-outlet></router-outlet>
-  </mat-sidenav-content>
-</mat-sidenav-container>
-
-
-  
-  
   `,
-  styleUrl: './app.component.scss'
-
-
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  @ViewChild(MatSidenav)
-  sidenav!: MatSidenav;
-}
+export class AppComponent {}
