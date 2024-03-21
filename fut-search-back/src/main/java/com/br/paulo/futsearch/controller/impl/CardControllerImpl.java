55051f5f93dcd19b5015042e8a0f19ae01de6e5e
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2024-03-21 20:32:25 -0300

    Refactoring back-end

--- fut-search-back/src/main/java/com/br/paulo/futsearch/controller/impl/CardControllerImpl.java
+++ fut-search-back/src/main/java/com/br/paulo/futsearch/controller/impl/CardControllerImpl.java
@@ -1,9 +1,7 @@
-package com.br.paulo.futsearch.resources;
+package com.br.paulo.futsearch.controller.impl;
 
 import com.br.paulo.futsearch.data.v1.CardVO;
-import com.br.paulo.futsearch.data.v1.NationVO;
-import com.br.paulo.futsearch.domain.Nation;
-import com.br.paulo.futsearch.services.NationService;
+import com.br.paulo.futsearch.services.CardService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -18,23 +16,26 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
+
 @RestController
-@RequestMapping(value = "/nations")
-@Tag(name = "Nation", description = "Endpoints for different Nations")
-public class NationResource {
+@RequestMapping(value = "/cards")
+@Tag(name = "Card", description = "Endpoints for Managing Card")
+public class CardController {
 
     @Autowired
-    private NationService service;
+    private CardService service;
+
+
 
     @GetMapping
-    @Operation(summary = "Finds all Nations", description = "Finds all Nations",
-            tags = {"Nation"},
+    @Operation(summary = "Finds all Cards", description = "Finds all Cards",
+            tags = {"Card"},
             responses = {
                     @ApiResponse(description = "Success", responseCode = "200",
                             content = {
                                     @Content(
                                             mediaType = "application/json",
-                                            array = @ArraySchema(schema = @Schema(implementation = NationVO.class))
+                                            array = @ArraySchema(schema = @Schema(implementation = CardVO.class))
                                     )
                             }),
                     @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
@@ -43,18 +44,18 @@ public class NationResource {
                     @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
             }
     )
-    public ResponseEntity<List<Nation>> findAll(){
-        List<Nation> list = service.findAll();
-
-        return ResponseEntity.ok().body(list);
+    public ResponseEntity<List<CardVO>> findAll(){
+        List<CardVO> listCard = service.findAll();
+        return ResponseEntity.ok().body(listCard);
     }
-    @GetMapping("/{nation}")
-    @Operation(summary = "Finds a Nation by Name", description = "Finds Nation by Name",
-            tags = {"Nation"},
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Finds a Card", description = "Finds a Card",
+            tags = {"Card"},
             responses = {
                     @ApiResponse(description = "Success", responseCode = "200",
                             content = {
-                                    @Content(schema = @Schema(implementation = NationVO.class))
+                                    @Content(schema = @Schema(implementation = CardVO.class))
                             }),
                     @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
                     @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
@@ -63,57 +64,56 @@ public class NationResource {
                     @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
             }
     )
-    public ResponseEntity<Nation> findByNation(@PathVariable("nation") String nation){
-        Nation obj = service.findByNation(nation);
+    public ResponseEntity<CardVO> findById(@PathVariable String id) {
+        CardVO obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
+
     @PostMapping
-    @Operation(summary = "Create a new Nation",
-            description = "Create a new Nation",
-            tags = {"Nation"},
+    @Operation(summary = "Create a new Card",
+            description = "Create a new Card",
+            tags = {"Card"},
             responses = {
                     @ApiResponse(description = "Success", responseCode = "200",
                             content = {
-                                    @Content(schema = @Schema(implementation = NationVO.class))
+                                    @Content(schema = @Schema(implementation = CardVO.class))
                             }),
                     @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
                     @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
                     @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
             })
-    public ResponseEntity<Void> create(@RequestBody Nation nation) {
-        Nation obj = nation;
-        obj = service.insert(obj);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+    public ResponseEntity<CardVO> create(@RequestBody CardVO card) {
+        CardVO obj = service.create(card);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
 
     @PutMapping("/{id}")
-    @Operation(summary = "Edit a existing Nation by ID",
-            description = "Edit a existing Nation by ID",
-            tags = {"Nation"},
-            responses = {
-                    @ApiResponse(description = "Success", responseCode = "200",
-                            content = {
-                                    @Content(schema = @Schema(implementation = NationVO.class))
-                            }),
-                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
-                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
-            })
-    public ResponseEntity<Void> update(@RequestBody Nation nation, @PathVariable String id) {
-        Nation obj = nation;
-        obj.setId(id);
-        obj = service.update(obj);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-        return ResponseEntity.noContent().build();
+    @Operation(summary = "Edit a existing Card",
+    description = "Edit a existing Card",
+    tags = {"Card"},
+    responses = {
+            @ApiResponse(description = "Success", responseCode = "200",
+                    content = {
+                            @Content(schema = @Schema(implementation = CardVO.class))
+                    }),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+    })
+    public ResponseEntity<CardVO> update(@RequestBody CardVO card, @PathVariable String id) {
+        CardVO obj = service.update(card);
+        return ResponseEntity.ok().body(obj);
     }
 
     @DeleteMapping("/{id}")
-    @Operation(summary = "Delete a Nation by ID",
-            description = "Delete a Nation by ID",
-            tags = {"Nation"},
+    @Operation(summary = "Delete a Card",
+            description = "Delete a card",
+            tags = {"Card"},
+            method = "DELETE",
             responses = {
                     @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
                     @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
@@ -121,8 +121,15 @@ public class NationResource {
                     @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
                     @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
             })
+
     public ResponseEntity<Void> delete(@PathVariable String id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+
+
+
+
+
 }
---
+++ fut-search-back/src/test/java/com/br/paulo/futsearch/services/CardServiceTest.java
@@ -0,0 +1,2 @@
+package com.br.paulo.futsearch.services;public class CardServiceTest {
+}
